dc7b1e1f74ff321278d942aa75fb04d2d8dcda38
Anton Shtipliyski <antonshtipliyski@yahoo.com>
2020-09-10 00:21:59 -0300

    Added props for Product component / Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.scss";
-import Product from "../Product/Product"
+import Product from "../Product/Product";
+import { v4 as uuidv4 } from "uuid";
 
 function Home() {
   return (
@@ -13,15 +14,52 @@ function Home() {
         />
       </div>
       <div className="home__row">
-        <Product />
+        <Product
+          id={uuidv4()}
+          title="The lean startup"
+          price={29.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+          rating={5}
+        />
+        <Product
+          id={uuidv4()}
+          title="Cuisinart SM-48RC Precision Master Petite 4.5 Qt Stand Mixer, Red"
+          price={247.58}
+          image="https://images-na.ssl-images-amazon.com/images/I/61sVtNJROSL._AC_SX522_.jpg"
+          rating={4}
+        />
       </div>
       <div className="home__row">
-        {/* Product */}
-        {/* Product */}
-        {/* Product */}
+        <Product
+          id={uuidv4()}
+          title="Fitbit Charge 4 fitness and Activity Tracker with Built-In Gps, Heart Rate, Black, One Size"
+          price={169.95}
+          image="https://images-na.ssl-images-amazon.com/images/I/71smqRr0pmL._AC_SX679_.jpg"
+          rating={4}
+        />
+        <Product
+          id={uuidv4()}
+          title="Echo Dot (3rd gen) - Smart speaker with Alexa - Charcoal"
+          price={39.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/419SVFEiGcL._AC_SY355_.jpg"
+          rating={5}
+        />
+        <Product
+          id={uuidv4()}
+          title="The lean startup"
+          price={29.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+          rating={5}
+        />
       </div>
       <div className="home__row">
-        {/* Product */}
+        <Product
+          id={uuidv4()}
+          title="The lean startup"
+          price={29.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+          rating={5}
+        />
       </div>
     </div>
   );
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import "./Product.scss"
+import { v4 as uuidv4 } from "uuid";
 
-function Product() {
+
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-        <span role="img" aria-label="star">⭐️</span>
+          {Array(rating).fill().map((_, i) => (
+            <span role="img" aria-label="star" key={uuidv4()}>⭐️</span>
+          ))}
         </div>
       </div>
-      <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt="cover"/>
+      <img src={image} alt="cover"/>
       <button>Add to basket</button>
     </div>
   )
